875a64898e92ed13f94dea1fb233b75f5c649d28
AtrashK <a.kuila@albynschool.org>
2025-10-02 09:10:38 +0100

    PUSHINGGGGGGGGGG

--- Records - BMI task.py
+++ Records - BMI task.py
@@ -10,3 +10,15 @@ BMIdetails = [person() for index in range(40)]
 BMIdetails[0].height = 1.75
 BMIdetails[0].weight = 70.5
 BMIdetails[0].bmi = BMIdetails[0].weight / (BMIdetails[0].height**2)
+
+BMIdetails[0].height = 1.62
+BMIdetails[0].weight = 55.8
+BMIdetails[0].bmi = BMIdetails[0].weight / (BMIdetails[0].height**2)
+
+BMIdetails[0].height = 1.88
+BMIdetails[0].weight = 95.2
+BMIdetails[0].bmi = BMIdetails[0].weight / (BMIdetails[0].height**2)
+
+BMIdetails[0].height = 1.70
+BMIdetails[0].weight = 
+BMIdetails[0].bmi = BMIdetails[0].weight / (BMIdetails[0].height**2)
