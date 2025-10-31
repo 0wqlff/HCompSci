383d6d1ae2defa4bdd3aef998f9d73797c1f7555
0wqlff <b.allan@albynschool.org>
2025-10-31 14:39:28 +0000

    my brackets are making a racket

---
+++ 805 - code tasks 1-4.py
@@ -0,0 +1,77 @@
+#QUESTIONS
+    #1a) John was found at 4
+    # b) Aakash
+
+    #2a) if goalsScored[i] <60:
+    #b if goalsScored[i] < 60 and goalsScored[i] > 40:
+
+#QUESTION 3
+# 805Task3.py
+# Standard Algorithms - Linear Search & Counting Occurrences
+
+# Investigate and modify
+
+# def getTargetCharacter():
+#   target = input("Enter the character you are looking for")
+
+#   return target
+
+# def getCharacters():
+#   characters = ["Desperate Dan", "Numbskulls", "Dennis the Menace", "Minnie the Minx", "Walter", "Gnasher", "Billy Whizz"]
+
+#   return characters
+
+# def findCharacterPosition(oneToFind):
+#   found = False
+#   foundPosition = -1
+#   for i in range(len(characters)):
+#     if characters[i]==oneToFind
+#         found=True
+#         foundPosition=i
+#   return foundPosition
+
+# #. *************MAIN*************
+
+# target = getTargetCharacter()
+# characters = getCharacters()
+# foundPosition = findCharacterPosition(target)
+
+# if found==True:
+#     print(target,"was found at position",foundPosition)
+# else:
+#     print(target,"was not found, womp womp")
+
+#QUESTION 4
+
+# def getTarget():
+#   targetAnimal = input("Please enter the target animal you are searching for")
+
+#   return targetAnimal
+
+# def getCircusAnimalsList():
+#   animals = ["giraffe", "chicken", "lion", "cheetah", "rat", "elephant", "snake", "chicken", "horse"]
+
+#   return animals
+
+# def countOccurrences(targetAnimal, animals):
+#     occurrences = 0
+#     for i in range(len(animals)):
+#         if animals[i]==targetAnimal:
+#             occurrences+=1
+#     return occurrences
+
+
+
+# def printOccurrences(occurrences):
+#   print(occurrences)
+
+
+# #*************  MAIN *************
+
+# targetAnimal = getTarget()
+# animals= getCircusAnimalsList()
+# occurrences = countOccurrences(targetAnimal, animals)
+# printOccurrences(occurrences)
+    
+    
+    
