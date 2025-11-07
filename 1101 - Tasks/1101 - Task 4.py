0f6e75f626d80b6e9abfceedf0a62bb098798b82
Aakash Kuila <A.Kuila@albynschool.org>
2025-11-07 14:39:28 +0000

    waffles?

--- 1101 - Tasks/1101 - Task 4.py
+++ 1101 - Tasks/1101 - Task 4.py
@@ -1,6 +1,7 @@
 names = ["John","Joan","Mark","Michael"]
+months = ["May","May","May","May","May"]
 ages = [23,35,23,8]
 
 with open("1101 - Tasks/names.txt","w") as wfile:
     for counter in range(0,len(names)):
-        wfile.write(names[counter] + ","+ str(ages[counter])+"\n")
+        wfile.write(names[counter] + "," + str(months[counter]) + "," + str(ages[counter])+"\n")
